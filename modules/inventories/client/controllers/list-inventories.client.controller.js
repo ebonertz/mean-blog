9eafebe4b0b34d946d161e44d838fec24917ccff
Evan Bonertz <bonert.unc@gmail.com>
2016-07-07 10:49:46 -0400

    Succesful inventory callback

--- modules/inventories/client/controllers/list-inventories.client.controller.js
+++ modules/inventories/client/controllers/list-inventories.client.controller.js
@@ -1,9 +1,30 @@
 (function () {
   'use strict';
+  var projectKey = "down-under";
+  var token = "awrARTgYvsFNDoclnsWoV95btThUCleM";
+  var baseURL = "https://api.sphere.io/$projectKey";
+  var tokenHeader = "Authorization: Bearer $token";
 
+
+
+  var inventories = angular.module('inventories');
+  
   angular
     .module('inventories')
     .controller('InventoriesListController', InventoriesListController);
+
+    inventories.controller('InventoriesListController',['$http', '$location', '$scope', function($http, $location, $scope) {
+            console.log('InventoriesListController ran');
+            $http.get('https://api.sphere.io/down-under/inventory?token=',
+            {headers: { Authorization: 'Bearer awrARTgYvsFNDoclnsWoV95btThUCleM'}})
+            .then(function successCallback(response){
+              $scope.inventory = response.data.results;
+              console.log(response);
+              console.log($scope.inventory);
+
+
+            });
+      }]);
 
   InventoriesListController.$inject = ['InventoriesService'];
 
@@ -12,4 +33,10 @@
 
     vm.inventories = InventoriesService.query();
   }
+
+
+
+
+
+
 })();
